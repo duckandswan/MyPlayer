bb8c416966a9eb60622ed9071361d84b1697f650
Song Bo <songbo@songs-macbook-pro.local>
2017-01-15 22:58:12 +0800

    debug

--- MyPlayer/MyPlayerController.swift
+++ MyPlayer/MyPlayerController.swift
@@ -9,26 +9,86 @@
 import UIKit
 import AVFoundation
 
-class MyPlayerController: UIViewController {
-
+class MyPlayerController: UIViewController,UITabBarDelegate,UITableViewDataSource {
+    @IBOutlet weak var playerView: UIView!
+    var player:AVPlayer!
+    @IBOutlet weak var subtitleTableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.frame.size.height = UIScreen.main.bounds.height / 2
+        view.frame.size.height = UIScreen.main.bounds.height
         view.frame.size.width = UIScreen.main.bounds.width
         view.backgroundColor = UIColor.green
-        addPlayer()
+        
+        subtitleTableView.register(UINib(nibName: "SubtitleCell", bundle: nil), forCellReuseIdentifier: "subtitle")
+        subtitleTableView.rowHeight = UITableViewAutomaticDimension
+        subtitleTableView.estimatedRowHeight = 100
     }
     
     func addPlayer(){
-        let url: URL = Bundle.main.url(forResource: "sample", withExtension: "mp4")!
-        //create player and player layer
-        let player = AVPlayer(url: url)
+//        let url: URL = Bundle.main.url(forResource: "sample", withExtension: "mp4")!
+        
+        let url: URL = Bundle.main.url(forResource: "冰血暴.Fargo.S02E01.中英字幕.WEB-HR.AAC.1024X576.x264", withExtension: "mp4")!
+        player = AVPlayer(url: url)
         let playerLayer = AVPlayerLayer(player: player)
-        //set player layer frame and attach it to our view
-        playerLayer.frame = view.bounds
-        view.layer.addSublayer(playerLayer)
-        //play the video
+        playerLayer.frame = playerView.bounds
+        playerView.layer.addSublayer(playerLayer)
         player.play()
+    }
+    
+    func addSubtitle(){
+        let url: URL = Bundle.main.url(forResource: "冰血暴.Fargo.S02E01.中英字幕.WEB-HR.AAC.1024X576.x264", withExtension: "srt")!
+
+        let str = try! String(contentsOf: url, encoding: .utf8)
+//        for c in str.characters {
+//            if c == "\n"{
+//                print("空格")
+//            }
+//            print(c)
+//        }
+//        print(str)
+        parseString(str: str)
+    }
+    var strs:[String] = []
+    func parseString(str:String){
+        let regex = try! NSRegularExpression(pattern: "^\\s+" , options: [.anchorsMatchLines])
+        let stop = "SomeStringThatYouDoNotExpectToOccurInSelf"
+        let range = NSRange(0..<str.utf16.count)
+        let modifiedString = regex.stringByReplacingMatches(in: str, options: [], range: range, withTemplate: stop)
+        strs = modifiedString.components(separatedBy: stop)
+        print(modifiedString)
+//        print(strs)
+//        for s in strs{
+//            print(s)
+//            print("SomeStringThatYouDoNotExpectToOccurInSelf")
+//        }
+
+    }
+    
+    @IBAction func tapPLayerView(_ sender: UITapGestureRecognizer) {
+        print("tap")
+        if player.rate != 0 {
+            player.pause()
+        }else{
+            player.play()
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return strs.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "subtitle", for: indexPath) as! SubtitleCell
+        cell.l.text = strs[indexPath.row]
+        return cell
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+//        addPlayer()
+        addSubtitle()
     }
 
     override func didReceiveMemoryWarning() {
---
+++ MyPlayer/SubtitleCell.swift
@@ -0,0 +1,26 @@
+//
+//  SubtitleCell.swift
+//  MyPlayer
+//
+//  Created by Song Bo on 15/01/2017.
+//  Copyright © 2017 Song Bo. All rights reserved.
+//
+
+import UIKit
+
+class SubtitleCell: UITableViewCell {
+
+    @IBOutlet weak var l: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
